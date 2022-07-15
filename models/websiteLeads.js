17bb92982f9ab027896de6c27f55fa18a8fdfb83
Gigsman2022 <adesh.gigsman@gmail.com>
2022-07-15 21:04:01 +0530

    Added services in the form

--- models/websiteLeads.js
+++ models/websiteLeads.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  service: { type: String, required: true },
+  name: { type: String },
+  phoneNumber: { type: String },
+  phone: { type: String },
+  service: { type: String },
+  subject: { type: String },
+  email: { type: String },
 });
 module.exports = mongoose.model("website_leads", Schema);
