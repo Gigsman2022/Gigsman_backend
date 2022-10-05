8164e2d2016ae23101ae57f4b4b15fb45795611a
Gigsman2022 <adesh.gigsman@gmail.com>
2022-10-05 21:09:16 +0530

    Cors

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
   );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    console.log("req is serving from cors");
     return res.status(200).json({});
   }
   next();
