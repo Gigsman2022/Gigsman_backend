a81ef15bcc6f8b69462f4b7c2d4046aea8efe3bc
Gigsman2022 <adesh.gigsman@gmail.com>
2022-10-08 12:59:29 +0530

    Timestamps in articles schema

--- models/article.js
+++ models/article.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
-let ArticleSchema = new mongoose.Schema({
-  text: String,
-  title: String,
-  description: String,
-  feature_img: String,
-  claps: Number,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  comments: [
-    {
-      author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-      text: String,
+let ArticleSchema = new mongoose.Schema(
+  {
+    text: String,
+    title: String,
+    description: String,
+    feature_img: String,
+    claps: Number,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-  ],
-});
+    comments: [
+      {
+        author: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 ArticleSchema.methods.clap = function () {
   this.claps++;
   return this.save();
